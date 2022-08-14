307b6b7940157ec11ddac5fe655227ef8cc7bba7
anthony-massaad <massaadanthony@hotmail.com>
2022-08-13 21:50:22 -0400

    Minor Details

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
+
 import Home from "./Home";
 import About from "./About";
 import ScrollToTop from "./utils/ScrollToTop";
@@ -7,16 +8,16 @@ import Projects from "./Projects";
 
 function App(){
 	return (
-		<Router>
+		<HashRouter>
 			<ScrollToTop>
 				<Routes>
 					<Route path="/" exact element={<Home />} />
-					<Route path="/about" exact element={<About />} />
-					<Route path="/project" exact element={<Projects />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/project" element={<Projects />} />
 					<Route path="*" element={<Home />} />
 				</Routes>
 			</ScrollToTop>
-		</Router>
+		</HashRouter>
 	);
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header(props){
     return (
         <header className="container container-larger">
-            <NavLink to="/" id="name" className="button-style-1 flex" area-hidden="true">A.M</NavLink>
+            <Link to="/" id="name" className="button-style-1 flex" area-hidden="true">A.M</Link>
             <nav>
                 <ul className="flex">
-                    {props.links.map((link, idx) => <li key={idx}><NavLink to={link.path}>{link.text}</NavLink></li>)}
+                    {props.links.map((link, idx) => <li key={idx}><Link to={link.path}>{link.text}</Link></li>)}
                 </ul>
             </nav>
         </header>
